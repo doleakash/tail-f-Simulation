01ba0dd44fcba6bd9bf9dce21c77054ec542fc6c
Akash <doleaakash@gmail.com>
2019-09-08 18:33:33 +0530

    appending log file and reading on change

--- index.js
+++ index.js
@@ -2,14 +2,24 @@ var express = require("express");
 var app = express();
 var fs = require('fs');
 
-fs.readFile('logger.txt', function (err, read) {
-  if (err) {
-    throw err;
-  } else {
-    console.log("read", read.toString())
+// to append logger.txt initally
+var stream = fs.createWriteStream("logger.txt");
+[...Array(10)].forEach(function () {
+  stream.write("welcome to simulation" + "\n");
+});
+stream.end();
 
-  }
-})
+
+// reading logger.txt  
+fs.watchFile("logger.txt", function () {
+  fs.readFile('logger.txt', function (err, read) {
+    if (err) {
+      throw err;
+    } else {
+      console.log("read", read.toString())
+    }
+  })
+});
 
 app.get("/", function (req, res) {
   console.log("data")
